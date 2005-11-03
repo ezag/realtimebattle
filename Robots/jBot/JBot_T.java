74d2fdecba5b4d3c36241b420962c6e15c34104c
vanicat <vanicat>
2005-11-03 16:18:15 +0000

    Adding GPL copyright notice to jBot by authorization of Ingo Beckmann

--- Robots/jBot/JBot_T.java
+++ Robots/jBot/JBot_T.java
@@ -3,10 +3,22 @@
 // JBot_T v0.2.0 in JAVA
 // Robot for RealTimeBattle v0.9.11
 // (c) 1999 by Ingo Beckmann
-// ingo.beckmann@fernuni-hagen.de
-// http://www.nnTec.de
-//
-//
+// ingoBeckmann@gmx.de
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
 import java.util.*;  
 import java.io.*;
 
